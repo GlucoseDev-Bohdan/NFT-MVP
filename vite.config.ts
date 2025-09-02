d22a0f86b0deb68fef4bf32edeb3a69f79592198
BohdanShcherbyna <shcherbynabohdan@outlook.com>
2025-09-02 17:20:24 +0300

    fix buffer issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
+    include: ['@solana/web3.js', 'buffer'],
     exclude: ['lucide-react'],
+  },
+  resolve: {
+    alias: {
+      buffer: 'buffer/',
+    },
   },
 });
